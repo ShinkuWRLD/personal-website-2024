39fb2f696cf2cc7199c1b59423f408f711f7de85
ShinkuWRLD <91349393+ShinkuWRLD@users.noreply.github.com>
2024-01-02 13:16:59 -0500

    installed vite, tailwind, and nextjs

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/tailwind.css";
+
+const HomePage: React.FC = () => {
+    return (
+        <div className="bg-gray-200 p-4"><h1 className="text-2x1 font-bold text-blue-500"> Hello from Tailwind</h1></div>
+    );
+};
+
+export default HomePage;
